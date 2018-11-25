aad40df98f66b645c9fdfbeae87696a2ffc888d5
HabibAroua <habibha.aroua82@gmail.com>
2018-11-25 19:50:58 +0100

    test the enverment

---
+++ src/redcompiler/RedCompiler.java
@@ -0,0 +1,18 @@
+package redcompiler;
+
+/**
+ *
+ * @author habib
+ */
+public class RedCompiler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+        System.out.println("We are going to create our fisrt compier");
+    }
+    
+}

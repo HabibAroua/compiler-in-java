4d7c952a053690902836101d78ba845ef206bab9
HabibAroua <habibha.aroua82@gmail.com>
2018-12-25 00:10:26 +0100

    dev class Array

--- src/symbol/Array.java
+++ src/symbol/Array.java
@@ -4,12 +4,20 @@
  * and open the template in the editor.
  */
 package symbol;
-
+import Lexer.Tag;
 /**
  *
  * @author habib
  */
-public class Array 
+public class Array extends Type 
 {
+    public final Type of;
+    public final int size;
     
+    public Array(int size , Type of)
+    {
+        super("[]",Tag.ARRAY_TYPE, size *of.width);
+        this.of=of;
+        this.size=size;
+    }
 }

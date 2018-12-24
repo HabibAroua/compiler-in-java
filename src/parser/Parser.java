37e25d128d4fcdfa21812cf6fc92397ab6287a5a
HabibAroua <habibha.aroua82@gmail.com>
2018-12-25 00:48:08 +0100

    create new class Parser

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -4,12 +4,53 @@
  * and open the template in the editor.
  */
 package parser;
-
+import Lexer.Lexer;
+import Lexer.Token;
+import java.io.IOException;
 /**
  *
  * @author habib
  */
 public class Parser 
 {
+    private final Lexer lexer;
+    private Token look;
     
+    public Parser(Lexer lexer) throws IOException
+    {
+        this.lexer=lexer;
+        move();
+    }
+    
+    private void move() throws IOException
+    {
+        look=lexer.scan();
+    }
+    
+    private void match(int t) throws IOException
+    {
+        if(look.tag==t)
+        {
+            move();
+        }
+        else
+        {
+            error("Syntax Error");
+        }
+    }
+    
+    private void error(String s)
+    {
+        throw new Error("new line "+lexer.line+" : "+s);
+    }
+    
+    public void start() throws IOException
+    {
+        program();
+    }
+    
+    public void program() throws IOException
+    {
+        block();
+    }
 }

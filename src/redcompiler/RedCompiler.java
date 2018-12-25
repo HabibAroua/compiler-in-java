b73b720ad205726166e8ac016d84ae3059aec8d2
HabibAroua <habibha.aroua82@gmail.com>
2018-12-25 22:21:22 +0100

    dev principal method

--- src/redcompiler/RedCompiler.java
+++ src/redcompiler/RedCompiler.java
@@ -1,18 +1,24 @@
 package redcompiler;
 import Lexer.Lexer;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import parser.Parser;
 /**
  *
- * @author habib
+ * @author Habib Aroua
  */
 public class RedCompiler {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
         // TODO code application logic here
+        System.setIn(new FileInputStream(new File(args[0])));
         Lexer lexer=new Lexer();
-    }
-    
+        Parser parser=new Parser(lexer);
+        parser.start();
+    }   
 }
